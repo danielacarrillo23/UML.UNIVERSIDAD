f317d1563abcd410accc0af07cf6ae8744a4872e
danielacarrillo23 <nindaniela233@hotmail.com>
2024-08-19 10:03:19 -0500

    Add files via upload

---
+++ FECHA.java
@@ -0,0 +1,71 @@
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 
+ */
+public class FECHA {
+
+    /**
+     * Default constructor
+     */
+    public FECHA() {
+    }
+
+    /**
+     * 
+     */
+    public Int Año;
+
+    /**
+     * 
+     */
+    public Int Mes;
+
+    /**
+     * 
+     */
+    public Int Dia;
+
+    /**
+     * 
+     */
+    public void Attribute4;
+
+    /**
+     * 
+     */
+    public void Editar() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Generar() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Ajustar() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Eliminar() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Mostrar() {
+        // TODO implement here
+    }
+
+}
