23ef9a8754a85f453df767c44a5bd344f991fa5c
danielacarrillo23 <nindaniela233@hotmail.com>
2024-08-19 10:03:42 -0500

    Add files via upload

---
+++ FRACCIONARIO.java
@@ -0,0 +1,68 @@
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 
+ */
+public class FRACCIONARIO {
+
+    /**
+     * Default constructor
+     */
+    public FRACCIONARIO() {
+    }
+
+    /**
+     * 
+     */
+    public Int Numerador;
+
+    /**
+     * 
+     */
+    public Int Denominador;
+
+    /**
+     * 
+     */
+    public void Fraccionario() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Agregar() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Editar() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Eliminar() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Numerador() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Denominador() {
+        // TODO implement here
+    }
+
+}
