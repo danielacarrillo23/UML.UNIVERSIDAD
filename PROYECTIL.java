99c64bd10e4310a33ebfa6467dbbb79438cb91aa
danielacarrillo23 <nindaniela233@hotmail.com>
2024-08-19 10:04:07 -0500

    Add files via upload

---
+++ PROYECTIL.java
@@ -0,0 +1,68 @@
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 
+ */
+public class PROYECTIL {
+
+    /**
+     * Default constructor
+     */
+    public PROYECTIL() {
+    }
+
+    /**
+     * 
+     */
+    public Double Velocidad;
+
+    /**
+     * 
+     */
+    public Double Angulo;
+
+    /**
+     * 
+     */
+    public void Datos() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Alcance() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void AlturaMaxima() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Tiempo() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Editar() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Calcular() {
+        // TODO implement here
+    }
+
+}
