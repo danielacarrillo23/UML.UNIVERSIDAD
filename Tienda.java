cf4d829a2f312aa307308a61a009729cc9296b9d
danielacarrillo23 <nindaniela233@hotmail.com>
2024-08-19 10:05:07 -0500

    Add files via upload

---
+++ Tienda.java
@@ -0,0 +1,85 @@
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 
+ */
+public class Tienda {
+
+    /**
+     * Default constructor
+     */
+    public Tienda() {
+    }
+
+    /**
+     * 
+     */
+    public String Nombre;
+
+    /**
+     * 
+     */
+    public String Direccion;
+
+    /**
+     * 
+     */
+    public String Telefono;
+
+    /**
+     * 
+     */
+    public List Productos;
+
+    /**
+     * 
+     */
+    public void ListaProductos() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Eliminar() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Crear() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Editar() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Añadir() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Operation6() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Operation7() {
+        // TODO implement here
+    }
+
+}
