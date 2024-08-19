5142bddaf9933b2e0c942bcf158ab581bbe1d4ec
danielacarrillo23 <nindaniela233@hotmail.com>
2024-08-19 10:04:24 -0500

    Add files via upload

---
+++ RELOJ.java
@@ -0,0 +1,73 @@
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 
+ */
+public class RELOJ {
+
+    /**
+     * Default constructor
+     */
+    public RELOJ() {
+    }
+
+    /**
+     * 
+     */
+    public Int Hora;
+
+    /**
+     * 
+     */
+    public Int Minutos;
+
+    /**
+     * 
+     */
+    public Int Segundos;
+
+    /**
+     * 
+     */
+    public void MostrarHora() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void EditarHora() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Ajustes() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Mostrar() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Operation5() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Operation6() {
+        // TODO implement here
+    }
+
+}
