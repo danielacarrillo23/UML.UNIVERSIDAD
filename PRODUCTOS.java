9db5cc155eda2af649f87cfd0c118136b90a07e1
danielacarrillo23 <nindaniela233@hotmail.com>
2024-08-19 10:04:54 -0500

    Add files via upload

---
+++ PRODUCTOS.java
@@ -0,0 +1,121 @@
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 
+ */
+public class PRODUCTOS {
+
+    /**
+     * Default constructor
+     */
+    public PRODUCTOS() {
+    }
+
+    /**
+     * 
+     */
+    public Int Codigo;
+
+    /**
+     * 
+     */
+    public String Nombre;
+
+    /**
+     * 
+     */
+    public Int Tipo;
+
+    /**
+     * 
+     */
+    public Date FechaExpiracion;
+
+    /**
+     * 
+     */
+    public String Fabricante;
+
+    /**
+     * 
+     */
+    public Int CantidadInventario;
+
+    /**
+     * 
+     */
+    public Double PrecioUnitario;
+
+    /**
+     * 
+     */
+    public void Datos() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Editar() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void TipoDeProductos() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Inventario() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Eliminar() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Añadir() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Lacteo() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Carnico() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Fruta() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Enlatado() {
+        // TODO implement here
+    }
+
+}
